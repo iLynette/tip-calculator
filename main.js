a4be68435e4f6af45acc364a56e8da248b9914d2
Lynette <lynetteacholah@icloud.com>
2020-08-13 19:33:42 +0300

    Added functionality

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const output = document.querySelector('.output');
 console.log(button);
 button.addEventListener('click', function(){
     const cost = document.querySelector('input');
-    console.log(cost.value*0.15);
-    output.innerText = 'output';
+    console.log(cost.value *0.15);
+    let tip = (cost.value *0.15).toFixed(2);
+    let temp = '<h1>You should tip Ksh'+tip+'on Ksh'+ cost.value +'</h1>';
+    output.innerHTML = temp;
 }) 
